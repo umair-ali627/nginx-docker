8b2fc3b09a03ebca073f64af6e045c43da1b9c8e
Ubuntu <ubuntu@ip-172-31-21-147.ec2.internal>
2025-07-17 20:23:51 +0000

    Update ports for nodeapp and pythonapp to avoid conflict

--- pythonapp/venv/bin/activate.fish
+++ pythonapp/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ubuntu/python-app/venv
+set -gx VIRTUAL_ENV /home/ubuntu/nginx-docker/pythonapp/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
